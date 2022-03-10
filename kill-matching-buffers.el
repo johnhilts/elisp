69eb9f341eff8fadafe9c457180f4981e1a95bca
John Hilts <johnhilts@live.com>
2022-03-09 18:48:45 -0800

    elisp - updates to killing newsticker buffers

--- kill-matching-buffers.el
+++ kill-matching-buffers.el
@@ -1,11 +1,21 @@
 (defun kill-matching-buffers (regex)
+  "kill any buffer that matches the supplied regex."
   (mapc
    #'(lambda (buffer)
-       (when (string-match regex (buffer-name buffer))
-         (kill-buffer (buffer-name buffer))))
+       (let ((buffer-name (buffer-name buffer)))
+         (when (string-match regex buffer-name)
+           (princ (format "Killing buffer: %s\n" buffer-name))
+           (kill-buffer buffer-name))))
    (buffer-list)))
 
 (defun kill-newsticker-buffers ()
+  "Kil certain buffers related to newsticker."
   (kill-matching-buffers "emacswiki")
   (kill-matching-buffers "hnrss")
   (kill-matching-buffers "ycombinator"))
+
+;;; example usage
+(progn
+  (princ (format "Buffer count (before) : %d\n" (length (buffer-list))))
+  (princ (kill-newsticker-buffers))
+  (princ (format "Buffer count (after) : %d\n" (length (buffer-list)))))
